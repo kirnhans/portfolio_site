b6a7f94edac589c99de4863104c8b59fb55ecdf2
Kirn Hans <kirn.hans@gmail.com>
2021-05-04 14:58:43 +0530

    Reorganizing files a bit and adding first drafts of GTS entries

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ from jinja2 import Template, Environment, FileSystemLoader
 from markdown import markdown
 
 
-standards_dict = { 1: ("Know students and how they learn", "templates/static/standard1.txt"),
-2: ("Know the content and how to teach it", "templates/static/standard2.txt"),
-3: ("Plan for and implement effective teaching and learning", "templates/static/standard3.txt"),
-4: ("Create and maintain supportive and safe learning environments", "templates/static/standard4.txt"),
-5: ("Assess, provide feedback and report on student learning", "templates/static/standard5.txt"),
-6: ("Engage in professional learning", "templates/static/standard6.txt"),
-7: ("Engage professionally with colleagues, parents/carers and the community", "templates/static/standard7.txt"),
+standards_dict = { 1: ("Know students and how they learn","templates/static/written_content/standard1.txt"),
+2: ("Know the content and how to teach it", "templates/static/written_content/standard2.txt"),
+3: ("Plan for and implement effective teaching and learning", "templates/static/written_content/standard3.txt"),
+4: ("Create and maintain supportive and safe learning environments", "templates/static/written_content/standard4.txt"),
+5: ("Assess, provide feedback and report on student learning", "templates/static/written_content/standard5.txt"),
+6: ("Engage in professional learning", "templates/static/written_content/standard6.txt"),
+7: ("Engage professionally with colleagues, parents/carers and the community", "templates/static/written_content/standard7.txt"),
 }
 
 class SiteGenerator(object):
@@ -43,6 +43,7 @@ class SiteGenerator(object):
             (head, essay_path) = standards_dict[num]
             essay_text = "This is an idea waiting to be explored in my journey as a teacher."
             if os.path.exists(essay_path):
+                print(essay_path)
                 essay_text = open(essay_path, "r").read()
             with open(os.path.join("public", f"standard{num}.html"), 'w+') as file:
                 html = template.render(standard_num=num, standard_head=head,
@@ -52,9 +53,14 @@ class SiteGenerator(object):
     def render_intro_page(self):
         print("Rendering intro pages to static file.")
         template = self.env.get_template('intro_template.html')
+        intro_text = "Hi! I'm Kirn."
+        intro_path = "templates/static/written_content/intro.txt"
+        if os.path.exists(intro_path):
+            intro_text = open(intro_path, "r").read()
         with open(os.path.join("public", "index.html"), 'w+') as file:
-            html = template.render()
+            html = template.render(intro_content=markdown(intro_text))
             file.write(html)
+
 
 if __name__ == "__main__":
     SiteGenerator()
