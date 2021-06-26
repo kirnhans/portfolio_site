c1bd8623b9779849290934be7373b53990686481
Kirn Hans <kirn.hans@gmail.com>
2021-06-26 13:16:22 +0530

    Using scaling instead of images of different sizes, flaps are more erratic now, thanks gill, this looks a lot better

--- templates/static/dev.js
+++ templates/static/dev.js
@@ -2,47 +2,62 @@ $('document').ready(function() {
     animate();
 });
 
-var pos = [
-"54px,130px,w1,0px,30px",
-"84px,130px,w2,30px,85px",
-"114px,130px,w3,60px,115px",
-"144px,130px,w4,90px,140px",
-"174px,130px,w5,120px,140px",
-"204px,130px,w6,150px,140px",
-"234px,130px,w5,180px,140px",
-"264px,130px,w4,210px,140px",
-"294px,130px,w3,240px,115px",
-"324px,130px,w2,270px,85px",
-"354px,130px,w1,300px,30px"
-];
-
-var step = 0;
-var laststep = pos.length;
 var timer;
 
 function animate()
 {
-   console.log("Hi");
-   document.getElementById("w1").style.visibility = "hidden";
-   document.getElementById("w2").style.visibility = "hidden";
-   document.getElementById("w3").style.visibility = "hidden";
-   document.getElementById("w4").style.visibility = "hidden";
-   document.getElementById("w5").style.visibility = "hidden";
-   document.getElementById("w6").style.visibility = "hidden";
+   const body = document.getElementById("body");
+   // body.style.right = "54px";
+   // body.style.bottom = "127px";
+   const wing = document.getElementById("w1");
+   // wing.style.right = "44px";
+   // wing.style.bottom = "150px";
 
-   if(step == laststep) step = 0;
+   const time = new Date().getTime();
+   const sine = Math.sin(time/300);
 
-   var newpos = pos[step].split(",");
+   wing.style.transform = `rotate(${Math.round(sine*2)}deg) scaleY(${sine})`;
+   // wing.style.transformOrigin = "left bottom";
+   body.style.transform = `rotate(${Math.round(sine*-5)}deg) translateY(${sine*2}px)`;
+   // body.style.transformOrigin = "45% 50%";
 
-   document.getElementById("body").style.left = newpos[0];
-   document.getElementById("body").style.top = newpos[1];
-   document.getElementById(newpos[2]).style.visibility = "visible";
-   document.getElementById(newpos[2]).style.left = newpos[3];
-   document.getElementById(newpos[2]).style.top = newpos[4];
-
-//   alert(step);
-
-   step++;
-   timer = setTimeout("animate()",300);
+   // monarchs flap their wings about 5 to 12 times a second
+   // source: https://journeynorth.org/tm/monarch/FlightPoweredSlowMotion.html
+   // var stepTime = getRandomArbitrary(83,200);
+   window.requestAnimationFrame(animate);
 }
 
+function butterfly_main() {
+   // randomly choose where to fly
+   // every [interval] landOnGlobe
+}
+
+function WingFlaps() {
+   // scale image, cycle through time
+   var stepTime = getRandomArbitrary(83,200);
+   timer = setTimeout("animate()",stepTime);
+}
+
+function flyHere(x, y) {
+   // get current position
+   // distance formula with x,y
+   // divide by speed of butterfly per millsecond
+   // loop
+   // set butterfly's position in next millisecond
+   // repeat until at position
+}
+
+function landOnGlobe() {
+   var globeTop = ["300px, 400px"]; //change this
+   // stop flying randomly
+   // set this as a location of where to go
+   // sit here and flap wings for a few seconds
+}
+
+// Mozilla Developer Network page:
+/**
+ * Returns a random number between min (inclusive) and max (exclusive)
+ */
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
