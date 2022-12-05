82ba3d0e1f022c34f415dfa826aefcbc90e2971a
Kirn Hans <kirn.hans@gmail.com>
2022-12-05 14:34:46 +0530

    Understood logic for butterfly wing location, turns out you can't add a string whoda thunk, fixed logic for changing location. Created helper functions - getter/setter butterfly body and for syncing the wing location with the body. Made an init method to put some logic which shouldn't have been in animate()

--- templates/static/dev.js
+++ templates/static/dev.js
@@ -1,37 +1,65 @@
 $('document').ready(function() {
-    animate();
+   init();
+
+   // animate();
+   flyTo(300, 400);
+   // flyTo(100, 300);
 });
 
+let butterflybody;
+let wing;
 let landing_zones = ["100px,400px", /*globe*/] //change this
 let timer;
 
-function animate()
-{
-   const body = document.getElementById("body");
-   // body.style.right = "54px";
-   body.style.bottom = "127px";
-   const wing = document.getElementById("w1");
-   // wing.style.right = "44px";
-   // wing.style.bottom = "150px";
-
+function init() {
+   butterflybody = document.getElementById("butterflybody");
+   
    // https://stackoverflow.com/questions/1100503/how-to-get-just-numeric-part-of-css-property-with-jquery
    // this method uses jQuery, with the value set in the css file
-   // doing this because I'm messing around with functions
-   let body_right = parseInt($(body).css('right'), 10 /* base*/);
-   // this method uses raw JS assuming body.style.bottom has been assigned earlier
-   // appropriate for when I implement the flying-to-other-places-with-location-
-   // assigned-by-JavaScript functionality
-   let body_bottom = parseInt(body.style.bottom, 10 /* base*/);
-   wing.style.right = `${body_right - 10}px`;
-   wing.style.bottom = `${body_bottom + 23}px`;
+   let butterflybody_right = parseInt($(butterflybody).css('right'), 10 /* base*/);
+   let butterflybody_bottom = parseInt($(butterflybody).css('bottom'), 10 /* base*/);
+
+   butterflybody.style.right = `${butterflybody_right}px`;
+   butterflybody.style.bottom = `${butterflybody_bottom}px`;
+
+   wing = document.getElementById("w1");
+   syncUpWing();
+
+   console.log()
+}
+
+/* Helper functions */
+function getButterflyBodyLoc() {
+   let butterflybody_right = parseInt(butterflybody.style.right, 10 /* base*/);
+   let butterflybody_bottom = parseInt(butterflybody.style.bottom, 10 /* base*/);
+   return [butterflybody_right, butterflybody_bottom];
+}
+
+function setButterflyBodyLoc(new_loc) {
+   butterflybody.style.right = `${new_loc[0]}px`;
+   butterflybody.style.bottom = `${new_loc[1]}px`;
+}
+
+function syncUpWing() {
+   let butterflybody_right = parseInt($(butterflybody).css('right'), 10 /* base*/);
+   let butterflybody_bottom = parseInt($(butterflybody).css('bottom'), 10 /* base*/);
+   
+   wing.style.right = `${butterflybody_right - 10}px`;
+   wing.style.bottom = `${butterflybody_bottom + 23}px`;
+}
+
+/* Doing Thing functions*
+function animate()
+{
+   syncUpWing();
 
    const time = new Date().getTime();
    const sine = Math.sin(time/300);
 
    wing.style.transform = `rotate(${Math.round(sine*2)}deg) scaleY(${sine})`;
    // wing.style.transformOrigin = "left bottom";
-   body.style.transform = `rotate(${Math.round(sine*2)}deg) translateY(${sine*2}px)`;
-   // body.style.transformOrigin = "45% 50%";
+   butterflybody.style.transform = `rotate(${Math.round(sine*2)}deg) translateY(${sine*2}px)`;
+   // butterflybody.style.transformOrigin = "45% 50%";
 
    // monarchs flap their wings about 5 to 12 times a second
    // source: https://journeynorth.org/tm/monarch/FlightPoweredSlowMotion.html
@@ -43,7 +71,7 @@ function butterfly_main() {
    // randomly choose where to fly
    // every [interval] landOnGlobe
    WingFlaps();
-   let prev_i = landing_zones.length();
+   let prev_i = -1; // impossible value, just so we have one
    while (true) {
       let i = getRandomArbitrary(0, landing_zones.length());
       if (i === prev_i) {
@@ -66,43 +94,51 @@ function WingFlaps() {
 }
 
 function flyTo(x, y) {
-   const body = document.getElementById("body");
+   const butterflybody = document.getElementById("butterflybody");
 
-   // get current position
-   // orig_loc: current location, defined by the right and bottom most pixels
-   // of the body of the butterfly, since these stay more fixed than the wings
-   let orig_loc = [body.style.right, body.style.bottom];
+   /* get current position
+   orig_loc: current location, defined by the right and bottom most pixels
+   of the body of the butterfly, since these stay more fixed than the wings */
+   let orig_loc = getButterflyBodyLoc();
 
-   // distance formula with x,y
-   // calculated in pixels
+   /* distance formula with x,y
+   calculated in pixels */
    let travel_distance = Math.hypot(x-orig_loc[0], y-orig_loc[1]);
-
-   // divide by speed of butterfly per millisecond
-   // butterflies fly upto 37 miles per hour
-   // or 16.5 metres / second
-   // So we'll say 100 pixels per millisecond?
-   // travel_time: in milliseconds, time from original location to x,y
+   // console.log("orig_loc", orig_loc);
+   // console.log(travel_distance)
+   
+   /* divide by speed of butterfly per millisecond
+   butterflies fly upto 37 miles per hour
+   or 16.5 metres / second
+   So we'll say 100 pixels per millisecond?
+   travel_time: in milliseconds, time from original location to x,y */
    let travel_time = travel_distance / 100;
+   // console.log(travel_time);
 
-   // loop
-   // set butterfly's position in next millisecond
-   // repeat until at position
-   for (let i = 0; i < travel_time; i++) {
+   /* loop
+   set butterfly's position in next millisecond
+   repeat until at position */
+   for (let i = 0.0; i < travel_time; i++) {
+      console.log("got here");
       let step_factor = i / travel_time;
+      console.log("step_factor", step_factor);
       let new_loc = [orig_loc[0] + (x - orig_loc[0]) * step_factor,
                      orig_loc[1] + (y - orig_loc[1]) * step_factor];
 
-      body.style.right = new_loc[0];
-      body.style.bottom = new_loc[1];
+      setButterflyBodyLoc(new_loc)
+      // butterflybody.style.right = new_loc[0];
+      // butterflybody.style.bottom = new_loc[1];
+      syncUpWing();
       //todo: delay 1 ms
    }
 
    //return value: is the butterfly more or less where I said it would be?
-   return ((x - body.style.right) < 5) && ((y - body.style.bottom) < 5)
+   return ((x - butterflybody.style.right) < 5) && ((y - butterflybody.style.bottom) < 5)
 }
 
 function landOn(x,y) {
-   let globeTop = ["300px, 400px"]; //change this
+   let globeTop = ["300, 400"]; //change this
+   let somePlace = ["500, 600"]
    // stop flying randomly
    // set this as a location of where to go
    // sit here and flap wings for a few seconds
@@ -114,4 +150,15 @@ function landOn(x,y) {
  */
 function getRandomArbitrary(min, max) {
     return Math.random() * (max - min) + min;
+}
+
+// This function is where I dump code instead of leaving it in other functions
+// wanna keep it in case I need the logic
+// don't wanna keep it in the function I got it from
+// halfway house! MariKondo your code!
+function dump() {
+   // this method uses raw JS assuming wing.style.bottom has been assigned earlier
+   // appropriate for when I implement the flying-to-other-places-with-location-
+   // assigned-by-JavaScript functionality
+   let wing_right = parseInt(wing.style.right, 10 /* base*/);
 }
