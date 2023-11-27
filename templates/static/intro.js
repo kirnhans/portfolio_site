5ccd717e845ce9290a50e9e61011d3a1d629e934
Kirn Hans <kirn.hans@gmail.com>
2023-11-28 10:50:24 +1100

    Changed and added some resources

--- templates/static/intro.js
+++ templates/static/intro.js
@@ -1,5 +1,6 @@
 $('document').ready(function() {
     setUpToggle();
+    lessonPlanFolderLift();
 });
 
 function setUpToggle() {
@@ -7,4 +8,18 @@ function setUpToggle() {
         location.href = "./developer.html";
     };
    document.getElementById("slider_button").checked = false;
+}
+
+/* this is for the lesson plan folder dropdown so that I can add more links
+    and it'll still go the right amount up  */
+function lessonPlanFolderLift() {
+
+    // there should be only one dropdown element with lesson-plan as a class
+    var dropdown_element = document.getElementsByClassName("lesson-plan")[0];
+    var a_elements = dropdown_element.children; 
+    
+    var moveTopBy = -1 * 16 * a_elements.length;
+    dropdown_element.style.top = `${moveTopBy}%`;
+
+    console.log(`${moveTopBy}%`);
 }
